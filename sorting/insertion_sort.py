c10c9d828d2e2526a077650ac202370a14478eed
Aram Kocharyan <akarmenia@gmail.com>
2013-02-10 17:45:55 +1100

    decreasing

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -1,11 +1,13 @@
-def insertion_sort(lst):
+def insertion_sort(lst, increasing=True):
 	for i in xrange(1,len(lst)):
 		key = lst[i]
 		j = i - 1;
-		while j >= 0 and lst[j] > key:
+		while j >= 0 and (lst[j] > key if increasing else lst[j] < key):
 			lst[j + 1] = lst[j]
 			j -= 1
 		lst[j + 1] = key
 	return lst
 
 print insertion_sort([3,2,5,1,4,3])
+
+print insertion_sort([3,2,5,1,4,3], increasing=False)
