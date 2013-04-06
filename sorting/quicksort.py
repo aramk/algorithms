6109585da08f5094f4eec54a5d4ca1b40bd50b11
Aram Kocharyan <akarmenia@gmail.com>
2013-04-07 01:20:38 +1100

    fixed quicksort, added kth largest

--- sorting/quicksort.py
+++ sorting/quicksort.py
@@ -1,25 +1,54 @@
-def quicksort_r(lst):
-	if len(lst) == 0:
-		return []
-	elif len(lst) == 1:
-		return lst
-	else:
-		(lst, pivot) = partition(lst, 0, len(lst))
-		left = quicksort_r(lst[:pivot])
-		right = quicksort_r(lst[pivot+1:])
-	return left + [lst[pivot]] + right
+import random, time, copy
 
-def partition(A, p, r):
-	pivotIndex = r-1
-	swapIndex = 0
-	for i in xrange(r-1):
-		if A[i] < A[pivotIndex]:
-			swap(A, swapIndex, i)
-			swapIndex += 1
-	swap(A, swapIndex, pivotIndex)
-	return (A, swapIndex)
+default_cmp = lambda x,y: x < y
+
+def quicksort_r(A, cmp = default_cmp, rand = False):
+	if len(A) == 0:
+		return []
+	elif len(A) == 1:
+		return A
+	else:
+		if rand:
+			pivotIndex = len(A) - 1
+			randIndex = random.randint(0, pivotIndex)
+			swap(A, randIndex, pivotIndex)
+		(A, pivot) = partition(A, 0, len(A), cmp)
+		left = quicksort_r(A[:pivot], cmp)
+		right = quicksort_r(A[pivot+1:], cmp)
+	return left + [A[pivot]] + right
+
+def partition(A, p, r, cmp = default_cmp):
+	if p == r-1:
+		return (A, p)
+	elif p > r-1 or len(A) == 0:
+		return (A, None)
+	else:
+		pivotIndex = r-1
+		swapIndex = p
+		for i in xrange(p, r-1):
+			if cmp(A[i], A[pivotIndex]):
+				swap(A, swapIndex, i)
+				swapIndex += 1
+		swap(A, swapIndex, pivotIndex)
+		return (A, swapIndex)
+
+def find_kth(A, k):
+	'''Find kth largest element in list A'''
+	return _find_kth(A, k, 0, len(A))
+
+def _find_kth(A, k, p, r):
+	(A, pivotIndex) = partition(A, p, r)
+	if k == pivotIndex:
+		return A[k]
+	elif (k < pivotIndex):
+		return _find_kth(A, k, p, pivotIndex)
+	else:
+		return _find_kth(A, k, pivotIndex + 1, r)
 
 def swap(A, i, j):
 	tmp = A[i]
 	A[i] = A[j]
 	A[j] = tmp
+
+
+print partition([5,4, 1, 3,2], 0, 5)
--- sorting/tests.py
+++ sorting/tests.py
@@ -5,16 +5,18 @@ from quicksort import *
 
 class BubbleTest(unittest.TestCase):
 	def runTest(self):
-		self.assertEqual(bubble_sort([3,2,4,1]), [1,2,3,4]);
+		self.assertEqual(bubble_sort([3,2,4,1]), [1,2,3,4])
 
 class SelectionTest(unittest.TestCase):
 	def runTest(self):
-		self.assertEqual(selection_sort([3,2,4,1]), [1,2,3,4]);
+		self.assertEqual(selection_sort([3,2,4,1]), [1,2,3,4])
 
 class QuickTest(unittest.TestCase):
 	def runTest(self):
-		self.assertEqual(quicksort_r([8,2,5,1,4,3]), [1,2,3,4,5,8]);
-
+		self.assertEqual(quicksort_r([8,2,5,1,4,3]), [1,2,3,4,5,8])
+		self.assertEqual(quicksort_r([3,1,4,6,2,5,3], lambda x,y: x > y), [6,5,4,3,3,2,1])
+		self.assertEqual(quicksort_r([3,1,4,6,2,5,3], lambda x,y: x > y), [6,5,4,3,3,2,1])
+		self.assertEqual(find_kth([2,5,8,3,7,9], 3), 7)
 
 if __name__ == '__main__':
     unittest.main()
