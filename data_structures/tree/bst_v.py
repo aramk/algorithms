7644ccc091ab3fd60fbf4416e6a793a979bff4a3
Aram Kocharyan <akarmenia@gmail.com>
2013-04-07 14:26:47 +1000

    delete

--- data_structures/tree/bst_v.py
+++ data_structures/tree/bst_v.py
@@ -1,78 +1,88 @@
+import random
+
 class BST:
 
-    # TODO remove the need to keep checking for None case of left and right nodes
+	# TODO remove the need to keep checking for None case of left and right nodes
 
-    def __init__(self, val=None):
-        self.left = None
-        self.right = None
-        self.val = val
-        self.parent = None
+	def __init__(self, val=None):
+		self.left = None
+		self.right = None
+		self.val = val
+		self.parent = None
 
-    def __str__(self):
-        return "[%s, %s, %s]" % (self.left, str(self.val), self.right)
+	def __str__(self):
+		return "[%s, %s, %s]" % (self.left, str(self.val), self.right)
 
-    def isEmpty(self):
-        return self.left == self.right == self.val is None
+	def isEmpty(self):
+		return self.left == self.right == self.val is None
 
-    def insert(self, val):
-        if self.isEmpty():
-            self.val = val
-        elif val == self.val:
-            return
-        elif val < self.val:
-            if self.left is None:
-                self.left = BST(val)
-                self.left.parent = self
-            else:
-                self.left.insert(val)
-        else:
-            if self.right is None:
-                self.right = BST(val)
-                self.right.parent = self
-            else:
-                self.right.insert(val)
+	def insert(self, val):
+		if self.isEmpty():
+			self.val = val
+		elif val == self.val:
+			return
+		elif val < self.val:
+			if self.left is None:
+				self.left = BST(val)
+				self.left.parent = self
+			else:
+				self.left.insert(val)
+		else:
+			if self.right is None:
+				self.right = BST(val)
+				self.right.parent = self
+			else:
+				self.right.insert(val)
 
-    def flatten(self):
-        if self.isEmpty():
-            return []
-        else:
-            left = [] if self.left is None else self.left.flatten()
-            right = [] if self.right is None else self.right.flatten()
-            return left + [self.val] + right
+	def flatten(self):
+		if self.isEmpty():
+			return []
+		else:
+			left = [] if self.left is None else self.left.flatten()
+			right = [] if self.right is None else self.right.flatten()
+			return left + [self.val] + right
 
-    def replace_in_parent(self, val=None):
-        if self.parent:
-            if self.parent.left == self:
-                self.parent.left = val
-            elif self.parent.right == self:
-                self.parent.right = val
-            if val:
-                val.parent = self.parent
+	def replace_in_parent(self, val=None):
+		if self.parent:
+			if self.parent.left == self:
+				self.parent.left = val
+			elif self.parent.right == self:
+				self.parent.right = val
+			if val:
+				val.parent = self.parent
 
-    def delete(self, val):
-        if val < self.val and self.left:
-            self.left.delete(val)
-        elif val > self.val and self.right:
-            self.right.delete(val)
-        else:
-            if self.left and self.right:
-                # TODO in-order ancestor/descendent (predecessor/successor)
-            elif self.left:
-                self.replace_in_parent(self.left)
-            elif self.right:
-                self.replace_in_parent(self.right)
-            else:
-                self.replace_in_parent(None)
+	def delete(self, val):
+		if val < self.val and self.left:
+			self.left.delete(val)
+		elif val > self.val and self.right:
+			self.right.delete(val)
+		else:
+			if self.left and self.right:
+				# TODO randomise with min and max
+				rand = random.randint(0, 1)
+				if rand == 0:
+					# predecessor
+					repl = self.left.max()
+				else:
+					# successor
+					repl = self.right.min()
+				repl.replace_in_parent(None)
+				self.val = repl.val
+			elif self.left:
+				self.replace_in_parent(self.left)
+			elif self.right:
+				self.replace_in_parent(self.right)
+			else:
+				self.replace_in_parent(None)
 
+	def min(self):
+		curr = self
+		while curr.left:
+			curr = curr.left
+		return curr
 
-
-a = BST(7)
-a.insert(5)
-a.insert(10)
-a.insert(4)
-a.insert(6)
-a.insert(9)
-a.insert(12)
-a.delete(4)
-print a
-
+	def max(self):
+		curr = self
+		while curr.right:
+			curr = curr.right
+		return curr
--- data_structures/tree/tests.py
+++ data_structures/tree/tests.py
@@ -9,6 +9,18 @@ class TreeTest(unittest.TestCase):
 		a.insert(3)
 		a.insert(0)
 		self.assertEqual(str(a), "[[None, 0, None], 1, [None, 2, [None, 3, None]]]")
+		a = BST(7)
+		a.insert(5)
+		a.insert(10)
+		a.insert(4)
+		a.insert(6)
+		a.insert(9)
+		a.insert(12)
+		a.delete(7)
+		predecessor = '[[[None, 4, None], 5, None], 6, [[None, 9, None], 10, [None, 12, None]]]'
+		successor = '[[[None, 4, None], 5, [None, 6, None]], 9, [None, 10, [None, 12, None]]]'
+		self.assertTrue(str(a) == predecessor or str(a) == successor)
+
 
 if __name__ == '__main__':
     unittest.main()
